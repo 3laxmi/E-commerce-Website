634c5550971da4f6c903eabf8a8a471c2fd115cf
Laxmi Tomar <laxmit4085@gmail.com>
2026-03-10 12:42:47 +0530

    Updated Home page and environment file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
-
 const Home = () => {
+
+  // const initRef = useRef(null)
+
+  // useEffect(() => {
+  //   initRef.current?.focus()
+  // }, [])
+
   return (
     <div>
       <Hero/>
       <LatestCollection/>
-      <BestSeller/>
+
+      <div ref={initRef} tabIndex={-1}>
+        <BestSeller/>
+      </div>
+
       <OurPolicy/>
       <NewsletterBox/>
-    
     </div>
   )
 }
