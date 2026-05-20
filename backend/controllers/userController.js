ec56afd93e9fb67ce1a7eef63473378424419ba5
Laxmi Tomar <laxmit4085@gmail.com>
2026-05-20 23:49:50 +0530

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -200,6 +200,7 @@ const adminLogin = async (req, res) => {
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASSWORD
